ba5de7dd303fee07bdcdaa0feec102b66c195756
Mohan5488 <krishnabudumuru7@gmail.com>
2025-03-21 06:23:17 +0530

    change back to sqlite

--- IPLROOM/settings.py
+++ IPLROOM/settings.py
@@ -103,14 +103,12 @@ WSGI_APPLICATION = "IPLROOM.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME":'IPL-HUB',
-        "USER":'postgres',
-        'PASSWORD':'1850',
-        'HOST':'localhost',
-        'PORT':'5432'
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
